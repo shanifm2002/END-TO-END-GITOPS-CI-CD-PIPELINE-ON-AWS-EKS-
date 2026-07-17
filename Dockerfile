2d8aa6114e080a3475e1c228e968d4731542af2c
Shanif M <shanifm2002@gmail.com>
2026-07-17 12:06:22 +0530

    Initial GitOps project

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# ==================================
+# Stage 1 - Build Application
+# ==================================
+FROM maven:3.9.9-eclipse-temurin-17 AS builder
+
+WORKDIR /app
+
+COPY pom.xml .
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+# ==================================
+# Stage 2 - Runtime Image
+# ==================================
+FROM eclipse-temurin:17-jre
+
+WORKDIR /app
+
+COPY --from=builder /app/target/gitops-demo-0.0.1-SNAPSHOT.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to GitOps CI/CD Pipeline on AWS EKS";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Healthy";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "Application Version : v1.0";
+    }
+}
